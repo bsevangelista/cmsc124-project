11f88bb521f9daa3e9c57d0302f1ef750d15a524
Genmaa <mtalarcon1@up.edu.ph>
2024-12-08 13:53:07 +0800

    fixed testcases for multiline comments

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -10,30 +10,37 @@ token_regex = re.compile('|'.join(token_patterns), re.S)
 def tokenize_lolcode(source_code):
     tokens = []
     line_number = 1
-
+    comment = False
+    start_line = 0
+    end_line = 0
+    
     for match in re.finditer(token_regex, source_code):
-        # print(match)
         kind = match.lastgroup # Type of token
-        # print(line_number,kind)
         value = match.group(kind) # Token
-        # print(line_number,value)
+
         if kind == 'WHITESPACE' or kind == 'NEWLINE':
             if kind == 'NEWLINE':
                 line_number += 1  # Track line numbers
                 tokens.append((kind, '\\n', line_number))
             continue  # Skip whitespace and newlines
-        elif kind in {'SINGLE_LINE_COMMENT', 'MULTI_LINE_COMMENT'}:
-            # Extract and process the comment text
-            if kind == 'SINGLE_LINE_COMMENT':
-                comment_text = value[4:]  # Skip "BTW"
-            elif kind == 'MULTI_LINE_COMMENT':
-                comment_text = value[4:-4]  # Skip "OBTW" and "TLDR"
-                # Count the number of newline characters in the multi-line comment
-                num_newlines = comment_text.count('\n')
-                line_number += num_newlines  # Increment line_number by the number of newlines
+        elif kind == 'SINGLE_LINE_COMMENT':
+            comment_text = value[4:]  # Skip "BTW"
+        elif kind == 'OBTW':
+            comment = True
+            start_line = line_number
+        elif kind == 'TLDR':
+            comment = False
+            end_line = line_number
         elif kind == 'MISMATCH':
-            print(f"Unexpected character {value} at line {line_number}")
+            if comment == False:
+                print(f"Unexpected character {value} at line {line_number}")
 
         else:
-            tokens.append((kind, value, line_number))
+            if comment == False:
+                tokens.append((kind, value, line_number))
+
+    for token in tokens:
+        # Check if a token is aligned with a multiline comment
+        if (start_line <= token[2] <= end_line) and token[0] != 'NEWLINE':
+            raise SyntaxError("Error: multiline comments should have its own line")
     return tokens
